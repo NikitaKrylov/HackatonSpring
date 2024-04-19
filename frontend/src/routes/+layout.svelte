488449633e16ab399751009d0cdf7544b70e7178
Kristina <kiegorova04@mail.ru>
2024-04-20 01:55:56 +0300

    sidebar-1

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import "../app.scss";
+    import SideBar from "./SideBar.svelte";
 </script>
 
 <slot />
+<main>
+    <SideBar/>
+</main>
---
+++ frontend/src/routes/SideBar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<div class="sidebar">
+    <div class="logo"></div>
+    <div class="sidebar__main">
+        <p>Основное</p>
+        <div class="main__btns">
+            <button class="btn-main">Главная</button>
+            <button class="btn-map">Карта</button>
+            <button class="btn-orders">Заказы</button>
+        </div>
+        <div class="line"></div>
+        <div class="main__panel-admin">
+            <button class="btn-users">Пользователи</button>
+            <button class="btn-roles">Роли</button>
+        </div>
+        <div class="main__user">
+            <img src="" alt="" class="avatar">
+            <div class="user-role">
+                <p class="user-name">Абдукарим Ашотович</p>
+                <p class="user-role">Администратор</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="scss"></style>
