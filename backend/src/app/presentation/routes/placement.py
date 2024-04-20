6bdb565a849ccfdcc9103e8609ce2b6a5e01cb6f
ParkieV <st-psk@mail.ru>
2024-04-20 22:24:03 +0300

    fix: fix placement

--- backend/src/app/presentation/routes/placement.py
+++ backend/src/app/presentation/routes/placement.py
@@ -1,3 +1,4 @@
+import random
 from fastapi import APIRouter, Depends
 
 from app.repository.placement import PlacementRepository
@@ -11,7 +12,14 @@ placement_repository = PlacementRepository()
 
 @router.get('', response_model=list[PlacementOutDTO])
 async def get_all_placements(filter_data: PlacementFilter = Depends(PlacementFilter)):
-    return await placement_repository.get_all(filter_data)
+
+    placements = await placement_repository.get_all(filter_data)
+    for placement in placements:
+        placement.coord = list(reversed([float(coord) for coord in placement.coord[1:-1].split(',')])) # type: ignore
+        if 1 <= placement.id <= 3:
+            placement.capacity = random.randint(2300, 2500)
+            placement.workload = random.randint(1000, 2000)
+    return placements
 
 
 @router.post('', response_model=PlacementOutDTO)
