686c47790dbba81b6686e2468ba8ebc3f3d541a2
Kristina <kiegorova04@mail.ru>
2024-04-20 05:20:57 +0300

    stores

--- frontend/src/routes/dashboard/Main/Store.svelte
+++ frontend/src/routes/dashboard/Main/Store.svelte
@@ -5,7 +5,7 @@
     <div class="busy">
         <p>Загруженность</p>
         <div class="busy-blocks">
-            <p class:active={false}>
+            <p class:active={true}>
             <p class:active={false}>
             <p class:active={false} >
         </div>
@@ -23,6 +23,7 @@
     }
 
     h3 {
+        font-size: 26px;
         margin-bottom: 20px;
     }
 
@@ -30,11 +31,34 @@
         display: inline-block;
     }
 
-    .busy-blocks p{
+    .busy {
         display: flex;
-        gap: 2px;
-        display: inline-block;
-        width: 17px;
-        border-top: 4px solid rgba(172, 210, 255, 1);
+        justify-content: center;
+        align-items: center;
+        gap: 18px;
+
+        .busy-blocks {
+            display: flex;
+            align-items:center;
+            gap: 4px;
+
+            p{
+                width: 17px;
+                height: 6px;
+                background-color: rgba(172, 210, 255, 1);
+
+                &:first-child{
+                    border-radius: 4px 0 0 4px;
+                }
+
+                &:last-child{
+                    border-radius: 0 4px 4px 0;
+                }
+
+                &.active {
+                    background-color: rgba(54, 207, 29, 1);
+                }
+            }
+        }
     }
 </style>
