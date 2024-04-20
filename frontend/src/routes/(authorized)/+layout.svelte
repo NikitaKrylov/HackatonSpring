3621bd811706e99703edc1035e2f9ba462121199
ClayenKitten <dima070704@yandex.ru>
2024-04-20 09:53:38 +0300

    refactor: move into subfolder

---
+++ frontend/src/routes/(authorized)/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import SideBar from "./SideBar.svelte";
+</script>
+
+<main>
+    <SideBar />
+    <slot />
+</main>
+
+<style>
+    main {
+        display: flex;
+        gap: 26px;
+        justify-content: space-between;
+    }
+</style>
