ee7fb6a49c8845974606fb8cf93db3184aadfc91
ClayenKitten <dima070704@yandex.ru>
2024-04-20 10:35:09 +0300

    fix: move body style into layout

--- frontend/src/routes/(authorized)/+layout.svelte
+++ frontend/src/routes/(authorized)/+layout.svelte
@@ -12,5 +12,9 @@
         display: flex;
         gap: 26px;
         justify-content: space-between;
+        padding: 30px 26px 20px;
+
+        color: rgba(46, 36, 77, 1);
+        background-color: rgba(243, 242, 244, 1);
     }
 </style>
