5cbd5c700488cb3ce027ec14e8a8d9c5b0610e6e
ClayenKitten <dima070704@yandex.ru>
2024-04-21 00:25:50 +0300

    add busy indicator

--- frontend/src/lib/components/Busy.svelte
+++ frontend/src/lib/components/Busy.svelte
@@ -1,33 +1,42 @@
-<script></script>
+<script lang="ts">
+    export let current: number;
+    export let maximum: number;
+
+    const off = "#acd2ff";
+    let color: string;
+    $: if (current / maximum > 0.66) {
+        color = "#F35757";
+    } else if (current / maximum > 0.33) {
+        color = "#FFC700";
+    } else {
+        color = "#36CF1D";
+    }
+</script>
 
 <div class="busy-blocks">
-    <p class:active={true}>
-    <p class:active={false}>
-    <p class:active={false} >
+    <p style:background-color={color}></p>
+    <p style:background-color={current / maximum > 0.33 ? color : off} />
+    <p style:background-color={current / maximum > 0.66 ? color : off} />
 </div>
 
 <style lang="scss">
     .busy-blocks {
-            display: flex;
-            align-items:center;
-            gap: 4px;
+        display: flex;
+        align-items: center;
+        gap: 4px;
 
-            p{
-                width: 17px;
-                height: 6px;
-                background-color: rgba(172, 210, 255, 1);
+        p {
+            width: 17px;
+            height: 6px;
+            background-color: #acd2ff;
 
-                &:first-child{
-                    border-radius: 4px 0 0 4px;
-                }
+            &:first-child {
+                border-radius: 4px 0 0 4px;
+            }
 
-                &:last-child{
-                    border-radius: 0 4px 4px 0;
-                }
-
-                &.active {
-                    background-color: rgba(54, 207, 29, 1);
-                }
+            &:last-child {
+                border-radius: 0 4px 4px 0;
             }
         }
+    }
 </style>
