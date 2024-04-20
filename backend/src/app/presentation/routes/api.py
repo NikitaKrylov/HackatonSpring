cd3f458850fbe71f506fd73485f6dc9d02914cad
ParkieV <st-psk@mail.ru>
2024-04-20 19:08:24 +0300

    feature: add purchase router

--- backend/src/app/presentation/routes/api.py
+++ backend/src/app/presentation/routes/api.py
@@ -8,6 +8,7 @@ from app.presentation.routes.role import router as role_router
 from app.presentation.routes.supply import router as supply_router
 from app.presentation.routes.user import router as user_router
 from app.presentation.routes.import_data import router as import_router
+from app.presentation.routes.purchase import router as purchase_router
 
 api_router = APIRouter()
 
@@ -18,3 +19,4 @@ api_router.include_router(supply_router, tags=['Поставки'])
 api_router.include_router(product_router, tags=['Продукты'])
 api_router.include_router(placement_router, tags=['Места'])
 api_router.include_router(import_router, tags=['Импорт'])
+api_router.include_router(purchase_router, tags=['Покупки'])
