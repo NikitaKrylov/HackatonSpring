6f5b2395f52cbacc7b9e340405283a64af0d9549
ParkieV <st-psk@mail.ru>
2024-04-20 13:55:18 +0300

    feature: add import_router

--- backend/src/app/presentation/routes/api.py
+++ backend/src/app/presentation/routes/api.py
@@ -7,6 +7,7 @@ from app.presentation.routes.purchase import router as purchase_router
 from app.presentation.routes.role import router as role_router
 from app.presentation.routes.supply import router as supply_router
 from app.presentation.routes.user import router as user_router
+from app.presentation.routes.import_data import router as import_router
 
 api_router = APIRouter()
 
@@ -16,3 +17,4 @@ api_router.include_router(purchase_router, tags=["Покупки"])
 api_router.include_router(supply_router, tags=['Поставки'])
 api_router.include_router(product_router, tags=['Продукты'])
 api_router.include_router(placement_router, tags=['Места'])
+api_router.include_router(import_router, tags=['Импорт'])
