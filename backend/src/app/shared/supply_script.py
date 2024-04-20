df4bc25c24280080d5af4745cfa5360dac380043
ParkieV <st-psk@mail.ru>
2024-04-20 10:19:44 +0300

    Feature: Added script to generate supplies

---
+++ backend/src/app/shared/supply_script.py
@@ -0,0 +1,37 @@
+
+import random
+from random import random
+
+from app.repository.supply import SupplyRepository
+from app.schemas.filters import PlacementFilter
+from backend.src.app.repository.placement import PlacementRepository
+
+
+def generate_supply(id: int):
+    storage_filter = PlacementFilter(placement_type="storage")
+    placement_repo = PlacementRepository()
+    storage_id = random.choice(placement_repo.get_all(storage_filter))[0]
+    supply = {"id": id,
+              "storage_id": storage_id}
+    return generate_offer(supply, storage_id)
+
+def _generate_placement_id(storage_id: int):
+    placement_id = storage_id
+    placement_repo = PlacementRepository()
+    while placement_id == storage_id:
+        placement_id = random.choice(placement_repo.get_all())[0]
+    return placement_id
+
+def generate_offer(supply: dict, storage_id: int):
+
+    supply["offers"] =  [{"id": i,
+                          "product_count": random.randint(1, 300),
+                          "product_id": random.choice(),
+                          "placement_id": _generate_placement_id(storage_id)} \
+                            for i in range(1, random.randint(2, 6))]
+    return supply
+
+def generate_supplies(count: int):
+    data = [generate_supply(i) for i in range(1, count + 1)]
+    # TODO: add CRUD for models
+    return data
