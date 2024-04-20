93ccfa8405263ce11a317a29ca20668776bcc470
ParkieV <st-psk@mail.ru>
2024-04-20 17:05:49 +0300

    fix: deleted distance DTO schemas

--- backend/src/app/schemas/placement.py
+++ backend/src/app/schemas/placement.py
@@ -5,6 +5,7 @@ class BasePlacementDTO(BaseModel):
     name: str
     coord: str
     placement_type: str
+    address: str
 
 
 class PlacementOutDTO(BasePlacementDTO):
@@ -12,21 +13,4 @@ class PlacementOutDTO(BasePlacementDTO):
 
 
 class PlacementCreateDTO(BasePlacementDTO):
-    pass
-
-
-class BaseDistanceDTO(BaseModel):
-    source_id: int
-    target_id: int
-    distance: float
-    duration: float
-
-
-class DistanceOutDTO(BaseDistanceDTO):
-    id: int
-    source: PlacementOutDTO
-    target: PlacementOutDTO
-
-
-class DistanceCreateDTO(BaseDistanceDTO):
     pass
