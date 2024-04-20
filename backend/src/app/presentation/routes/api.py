552dd73bb8da7d67279eb07e6253580cdf47fec6
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-04-20 12:17:00 +0300

    fix(api): remove '/api' prefix from api router

--- backend/src/app/presentation/routes/api.py
+++ backend/src/app/presentation/routes/api.py
@@ -8,7 +8,7 @@ from app.presentation.routes.role import router as role_router
 from app.presentation.routes.supply import router as supply_router
 from app.presentation.routes.user import router as user_router
 
-api_router = APIRouter(prefix="/api")
+api_router = APIRouter()
 
 api_router.include_router(user_router, tags=["Пользователь"])
 api_router.include_router(role_router, tags=['Роли'])
