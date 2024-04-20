bbad51d5abad73d21f7594fb65fce152f14eecdc
ParkieV <st-psk@mail.ru>
2024-04-20 19:58:46 +0300

    feature: create supply endpoint

--- backend/src/app/presentation/routes/supply.py
+++ backend/src/app/presentation/routes/supply.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from app.repository.supply import SupplyRepository
 from app.schemas.filters import SupplyFilter
 from app.schemas.supply import SupplyOutDTO
+from app.shared.supply_script import generate_supplies
 
 router = APIRouter(prefix="/supplies")
 
@@ -14,7 +15,6 @@ async def get_all_supplies(filter_data: SupplyFilter = Depends(SupplyFilter)):
     return await supply_repository.get_all()
 
 
-@router.post('')
+@router.post('', tags=["Для генерации путей в БД"])
 async def create_supply(data):
-    # TODO сделать после того как сделаем метод создания в репозитории
-    pass
+    await generate_supplies(40)
