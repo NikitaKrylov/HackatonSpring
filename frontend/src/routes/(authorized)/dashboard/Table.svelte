a524ebc21cd56464e45bdb87f9c42292b297148c
ClayenKitten <dima070704@yandex.ru>
2024-04-21 13:41:00 +0300

    final

--- frontend/src/routes/(authorized)/dashboard/Table.svelte
+++ frontend/src/routes/(authorized)/dashboard/Table.svelte
@@ -7,8 +7,8 @@
 <section>
     <header>
         <h2>Последние заявки</h2>
+        <a class="seealso" href="/supply">Смотреть всё</a>
     </header>
-
     <ul>
         <li class="li-header">
             <p>№</p>
@@ -35,18 +35,41 @@
     section {
         flex: 1px;
         background: var(--white);
-        padding: 35px;
+        padding: 0 35px;
         border-radius: 35px;
         max-height: 27rem;
     }
 
     header {
-        margin-bottom: 46px;
+        display: flex;
+        margin-bottom: 22px;
+        align-items: center;
 
         h2 {
             font-weight: 700;
             font-size: 30px;
             margin-bottom: 8px;
+        }
+        .seealso {
+            display: block;
+            color: var(--egg-blue-100);
+            position: relative;
+            margin-bottom: 16px;
+            padding-right: 14px;
+            text-decoration: none;
+            margin-left: auto;
+
+            &::after {
+                content: "";
+                position: absolute;
+                display: inline-block;
+                background-image: url("/icons/arrow-right.svg");
+                background-repeat: no-repeat;
+                width: 7px;
+                height: 14px;
+                right: 0;
+                top: 4px;
+            }
         }
     }
 
