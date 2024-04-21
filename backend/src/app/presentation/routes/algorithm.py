4b0aa00222c44ae19b9c0f3a5deeb166ab5b3798
ParkieV <st-psk@mail.ru>
2024-04-21 03:34:35 +0300

    fix: fix endpoint logic

--- backend/src/app/presentation/routes/algorithm.py
+++ backend/src/app/presentation/routes/algorithm.py
@@ -15,7 +15,7 @@ async def optimal_path(supply_id: int, num_trucks: int = 1):
     supply = await _supply_repository.get_supply_by_id(supply_id)
     storage_id = supply.id - 1 # type: ignore
     clients_id = [i.placement_id - 1 for i in supply.offers] # type: ignore
-    if len(clients_id) > num_trucks:
+    if len(clients_id) < num_trucks:
         raise HTTPException(status_code=400, detail="Number of trucks must be greater than or equal to the number of clients")
 
     with open(os.path.abspath("data/points.geojson"), 'r') as f:
@@ -24,4 +24,7 @@ async def optimal_path(supply_id: int, num_trucks: int = 1):
     with open(os.path.abspath("data/routes.json"), 'r') as f:
         routes_json = json.load(f)
 
-    return await get_routes(storage_id, clients_id, points_json, routes_json, num_trucks=num_trucks)
+    result = await get_routes(storage_id, clients_id, points_json, routes_json, num_trucks=num_trucks)
+    print([[j+1 for j in i] for i in result])
+    result =[[j+1 for j in i] for i in result]
+    return result
