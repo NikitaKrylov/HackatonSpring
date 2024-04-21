c6cb5607e43281491caf85d0f660235d601ca2d5
ClayenKitten <dima070704@yandex.ru>
2024-04-21 09:24:22 +0300

    fix index in supply

--- frontend/src/routes/(authorized)/supply/List.svelte
+++ frontend/src/routes/(authorized)/supply/List.svelte
@@ -1,39 +1,25 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import {fetchOffers} from './../../../lib/data/offer'
+    import { fetchOffers, type Offer } from "$lib/data/offer";
 
-    let items = [];
-
+    let items: Offer[] = [];
     onMount(async () => {
-        items = await fetchOffers()
-        console.log(items);
-        
-    } )
-
-    // const items = [
-    //     {
-    //         num: 2000,
-    //         position: 'apples',
-    //         id: 1,
-    //         order: 'pyaterochka',
-    //         address: 'улица Ленинский проспект, дом 52, корпус 3',
-    //         count: 120
-    //     }
-    // ]
+        items = await fetchOffers();
+    });
 </script>
 
 <ul>
     <li class="li-header">
-        <p>№ заявки</p>
+        <p>№</p>
         <p>Позиция</p>
         <p>Id товара</p>
         <p>Заказчик</p>
         <p>Адрес</p>
-        <p>Количество </p>
+        <p>Количество</p>
     </li>
     {#each items as item, i}
         <li>
-            <p>{item.supply_id}</p>
+            <p>{i + 1}</p>
             <p>{item.product.name}</p>
             <p>{item.product.sku}</p>
             <p>{item.product.manufactor}</p>
@@ -44,38 +30,37 @@
 </ul>
 
 <style lang="scss">
-    ul{
+    ul {
         max-width: 100%;
         max-height: 86.5%;
         overflow: auto;
 
-        li{
+        li {
             border: 1px solid var(--dark-blue-40);
             padding: 33px 0;
             display: grid;
             grid-template-columns: 130px 200px 126px 300px 295px auto;
 
-            &:first-child{
+            &:first-child {
                 padding: 0 0 33px;
                 border: none;
             }
 
-            p:nth-child(1){
+            p:nth-child(1) {
                 padding-left: 15px;
             }
 
-            p:nth-child(4){
+            p:nth-child(4) {
                 padding-right: 8px;
             }
 
-            p:nth-child(5){
+            p:nth-child(5) {
                 max-width: 250px;
             }
-            
-            p:nth-child(6){
+
+            p:nth-child(6) {
                 text-align: center;
             }
-
         }
     }
 
@@ -86,12 +71,10 @@
         background-color: var(--egg-blue-90);
     }
 
-    ::-webkit-scrollbar-track{
+    ::-webkit-scrollbar-track {
         width: 2px;
         height: 100%;
         border-radius: 100px;
         background-color: var(--egg-blue-0);
     }
-
-
 </style>
