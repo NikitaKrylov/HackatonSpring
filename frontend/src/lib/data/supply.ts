e66fb0891f3f1ad51a24ad34631ddd0c7cfbcba1
ClayenKitten <dima070704@yandex.ru>
2024-04-21 03:39:57 +0300

    add models and fetch functions for main datatypes

--- frontend/src/lib/data/supply.ts
+++ frontend/src/lib/data/supply.ts
@@ -1,9 +1,26 @@
-import type { Offer } from "./offer"
+import { fetchJSON } from "$lib/fetch";
 
 export type Supply = {
-  id: number
-  storage_id: number
-  offers: Offer[]
-  created_at: string
-  supply_status: string
+    id: number;
+    storage_id: number;
+    offers: {
+        id: number;
+        product_count: number;
+        product_id: number;
+        placement_id: number;
+        supply_id: number;
+    }[];
+    storage: {
+        id: number;
+        address: string;
+        name: string;
+        coord: string;
+        placement_type: string;
+    };
+    created_at: string;
+    supply_status: string;
+};
+
+export async function fetchSupplies(): Promise<Supply[]> {
+    return fetchJSON<Supply[]>("/supplies").then(x => x.data);
 }
